d25d39a6c9941a76b30fe734df4997f8b55b14f7
Marco Vavassori <mvavassori@abacusnext.com>
2023-04-19 12:43:52 +0100

    Initiate Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();    // bringing in framework services that enable MVC in this app
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();    // looking for requests for static files: html, css, etc.
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();    // contains info that user shouldn't see but that can help us developers
+
+app.MapDefaultControllerRoute();    // needed to be able to navigate (route) to our pages (views), it is endpoint middleware
 
 app.Run();
